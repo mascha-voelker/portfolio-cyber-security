2f73ec3fc2e3b5ee3a3901269ad3d4320e7db602
mascha-voelker <mascha.volker@gmail.com>
2025-07-13 12:41:00 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    // No api configuration here
+  reactStrictMode: true,
+  experimental: {
+    outputFileTracingRoot: '.'
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
